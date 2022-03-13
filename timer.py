e90bf443a309c477ac2f0b7c84abd31067ea7543
jaimebriesemeister <82653221+jaimebriesemeister@users.noreply.github.com>
2022-03-13 09:54:11 -0300

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,28 @@
+# impore os módulos time e playsound
+import time
+from playsound import playsound 
+  
+# defina a função de contagem regressiva do cronômetro
+def countdown_timer(seconds):
+    
+    while seconds > 0:       
+
+        mins = int(seconds / 60)
+        secs = int(seconds % 60)
+
+        timer = f'{mins}:{secs}'
+
+        print(timer)
+        
+        seconds -= 1
+      
+    print('Tempo Esgotado!')
+
+    playsound('mixkit-sound.wav')
+  
+  
+# input time in seconds
+seconds = input("Digite o tempo em segundos: ")
+  
+# function call
+countdown_timer(int(seconds))
